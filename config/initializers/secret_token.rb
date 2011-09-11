2468b3ede28311e9a0d6212e49c2e95b9daba6ea
Carol Bartram <info@mycheaperutilitybills.co.uk>
2011-09-11 16:36:16 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'a00a9b6b02b4b4a26eaaa4ce79d406e3075fee107ba3a9c86964b4f7746bac82211d59de60a8ccb434ebc0b8d5852ed5715dc30e08e7ecc467142976eada7155'
